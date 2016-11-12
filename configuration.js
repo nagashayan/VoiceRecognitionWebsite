cb1a978dd2d49877cb4c191a83da0d998728a5f4
Su Park <andjs1012@gmail.com>
2016-11-12 18:07:30 -0500

    model code

--- configuration.js
+++ configuration.js
@@ -2,10 +2,10 @@
 
 var URL = 'wss://ws.dev.nuance.com/?';
 
-var APP_ID = "NMDPTRIAL_biswal_sid_gmail_com20161110092132";
-var APP_KEY = "67ca8cc1e4baccb25db67fc777da06a9809df865981c510b44989f530a045b71cee732b2a1ff65ffb5e4c62dbfcdd63f6acea502eaf60d2e8fac8199b1dc0a7f";
+var APP_ID = "NMDPTRIAL_andjs1012_gmail_com20161112124814";
+var APP_KEY = "d8081d9ba0f490b6215c0af9563a55151f102daebb9387ee7bdfa2ebcaddd4a2e5f23e85e6ce3b072a4500817f55969e65c44c2057d30fd2b2b1bd7d8e921f73";
 var USER_ID = "";
-var NLU_TAG = "M4251_A2049_V1";
+var NLU_TAG = "M4272_A2058_V1";
 
 // ASR
 // See: https://developer.nuance.com/public/index.php?task=supportedLanguages
