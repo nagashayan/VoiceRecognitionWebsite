2739e8c071f66ff5dbe19d40b1c65c56023c9101
Nagashayan <nagashayan1@gmail.com>
2016-11-12 15:13:48 -0500

    init commit
    sample appication generated by nuance

---
+++ configuration.js
@@ -0,0 +1,17 @@
+// Credentials
+
+var URL = 'wss://ws.dev.nuance.com/?';
+
+var APP_ID = "NMDPTRIAL_biswal_sid_gmail_com20161110092132";
+var APP_KEY = "67ca8cc1e4baccb25db67fc777da06a9809df865981c510b44989f530a045b71cee732b2a1ff65ffb5e4c62dbfcdd63f6acea502eaf60d2e8fac8199b1dc0a7f";
+var USER_ID = "";
+var NLU_TAG = "M4251_A2049_V1";
+
+// ASR
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var ASR_LANGUAGE = "eng-USA";
+
+// TTS
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var TTS_LANGUAGE = "eng-USA";
+var TTS_VOICE = "";
